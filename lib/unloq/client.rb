173cb6a4b385a802f374958a03f2b548badb46e9
Matt Mueller <muellermr@gmail.com>
2014-08-20 11:11:18 -0400

    Instantiate an Unloq::Client instance using named arguments.
    This is so that we can return friendlier errors if they do it wrong.

--- lib/unloq/client.rb
+++ lib/unloq/client.rb
@@ -1,12 +1,22 @@
 module Unloq
   class Client
 
-    attr_reader :api_key
+    attr_reader :api_key, :namespace
 
-    def initialize api_key = nil
-      raise ArgumentError.new("Please include your api key, e.g. Unloq::Client.new(API_KEY)") unless api_key
-      @api_key = api_key
+    # Initialize the client class that will be used for all Unloq interactions.
+    #
+    # @param [String] api_key Your Unloq api key.
+    # @param [String] namespace The namespace under which Unloq events should be created, e.g. appname-dev
+
+    def initialize api_key: nil, namespace: nil
+      unless api_key && namespace
+        raise ArgumentError.new("You must include both an api_key and namespace, e.g. Unloq::Client.new(api_key: 'foo', namespace: 'bar')")
+      end
+
+      @api_key   = api_key
+      @namespace = namespace
     end
+
 
   end
 end
--- spec/unloq/client_spec.rb
+++ spec/unloq/client_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 describe Unloq::Client, '.new' do
   
-  it "should raise an error if an api key is not passed" do
+  it "should raise an error if no arguments are passed" do
     expect {
       Unloq::Client.new
-    }.to raise_error(ArgumentError, /include your api key/)
+    }.to raise_error(ArgumentError, /must include both an api_key and namespace/)
   end
 
-  it "should instantiate happily with an api key" do
-    expect(Unloq::Client.new('api-key')).to be_instance_of(Unloq::Client)
+  it "should raise an error if only an api key is passed" do
+    expect {
+      Unloq::Client.new(api_key: 'foo')
+    }.to raise_error(ArgumentError, /must include both an api_key and namespace/)
+  end
+
+  it "should raise an error if only namespace is passed" do
+    expect {
+      Unloq::Client.new(namespace: 'app-dev')
+    }.to raise_error(ArgumentError, /must include both an api_key and namespace/)
+  end
+
+  it "should instantiate happily with an api key and namespace" do
+    expect(Unloq::Client.new(api_key: 'api-key', namespace: 'app-dev')).to be_instance_of(Unloq::Client)
   end
 end
