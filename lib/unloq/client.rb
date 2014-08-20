db490ac39223fdd268479243f601c200adf452c5
Matt Mueller <muellermr@gmail.com>
2014-08-20 10:19:09 -0400

    Unloq::Client - instantiate with an api key.

--- lib/unloq/client.rb
+++ lib/unloq/client.rb
@@ -1,5 +1,12 @@
 module Unloq
   class Client
 
+    attr_reader :api_key
+
+    def initialize api_key = nil
+      raise ArgumentError.new("Please include your api key, e.g. Unloq::Client.new(API_KEY)") unless api_key
+      @api_key = api_key
+    end
+
   end
 end
--- spec/unloq/client_spec.rb
+++ spec/unloq/client_spec.rb
@@ -2,7 +2,13 @@ require 'spec_helper'
 
 describe Unloq::Client, '.new' do
   
-  it "should respond to new" do
-    expect(Unloq::Client.respond_to?(:new)).to eq(true)
+  it "should raise an error if an api key is not passed" do
+    expect {
+      Unloq::Client.new
+    }.to raise_error(ArgumentError, /include your api key/)
+  end
+
+  it "should instantiate happily with an api key" do
+    expect(Unloq::Client.new('api-key')).to be_instance_of(Unloq::Client)
   end
 end
